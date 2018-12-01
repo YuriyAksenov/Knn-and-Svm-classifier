e76d8d3f9563484d81e903649dd92fa9a35fd577
Yuriy Aksenov <Yura290610@yandex.ru>
2018-12-01 17:46:21 +0300

    Добавил основной класс Точка данных

--- Point.py
+++ Point.py
@@ -1,16 +1,13 @@
 class Point:
-    
+
     x: float
     y: float
-    category: int
+    label: int
 
-    def __init__(self, x: float, y: float, category: int):
+    def __init__(self, x: float, y: float, label: int):
         self.x = float(x)
         self.y = float(y)
-        self.category = category
-
-    def get_distance(self, other_point: Point):
-        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+        self.label = label
 
     def __str__(self):
-        return "{x:%f,y:%f,c:%s}" % (self.x, self.y, self.category)
+        return "{x:%f,y:%f,c:%s}" % (self.x, self.y, self.label)
