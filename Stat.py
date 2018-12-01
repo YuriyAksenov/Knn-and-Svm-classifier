269dba6880f27135e0b3d02959fa1a6e775b135a
Yuriy Aksenov <Yura290610@yandex.ru>
2018-12-01 22:10:03 +0300

    Изменил статистику

--- Stat.py
+++ Stat.py
@@ -1,17 +1,20 @@
 class Stat:
-    def __init__(self, folders_count: int, neighbors_count, power, kernel, k1, k2, coordinate_system, accuracy, f_score):
-        self.folders_count = folders_count
-        self.neighbors_count = neighbors_count
+    def __init__(self, numFolds, numNeighbor, power, kernel, k1, k2, coordinateSystem, accuracy, f_score):
+        self.numFolds = numFolds
+        self.numNeighbor = numNeighbor
         self.kernel = kernel
         self.power = power
         self.k1 = k1
         self.k2 = k2
-        self.coordinate_system = coordinate_system
+        self.coordinateSystem = coordinateSystem
         self.accuracy = accuracy
         self.f_score = f_score
-
+    
+    # def __str__(self):
+    #     main = "| %d | %d | %d | %s | %0.4f | %0.4f | %s " % (self.numFolds, self.numNeighbor, self.power, self.kernel, self.k1, self.k2, self.coordinateSystem)
+    #     params = "| %0.4f | %0.4f |" % (self.accuracy, self.f_score)
+    #     return  main + params 
     def __str__(self):
-        main = "| %d | %d | %d | %s | %0.4f | %0.4f | %s " % (
-            self.folders_count, self.neighbors_count, self.power, self.kernel, self.k1, self.k2, self.coordinate_system)
-        params = "| %0.4f | %0.4f |" % (self.accuracy, self.f_score)
-        return main + params
+        main = "| numFold:%d | numNeighbor:%d | power:%d | kernel:%s | %s " % (self.numFolds, self.numNeighbor, self.power, self.kernel, self.coordinateSystem)
+        params = "| %0.4f |" % (self.f_score)
+        return  main + params 
