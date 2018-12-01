f25a0113ed7f3c9843416df886ab81277f00952f
Yuriy Aksenov <Yura290610@yandex.ru>
2018-12-01 17:46:35 +0300

    Общий класс по выводу данных, чтению данных

--- Data.py
+++ Data.py
@@ -1,42 +1,56 @@
-import csv
 from typing import List
 import numpy as np
 import matplotlib.pyplot as pl
 from matplotlib.colors import ListedColormap
 
 from Point import Point
+from CsvReader import Csv
 
 
 class Data:
 
     @staticmethod
-    def find_closest( points:List[Point], point: Point, count: int):
-        distances = map(
-            lambda point: {"point": point, "distance": point.distance(point)}, points)
-        distances = sorted(
-            distances, key=lambda distance: distance["distance"])
-        return list(map(lambda distance: distance["point"], distances[:count]))
-
-    @staticmethod
     def read_points_from_file(file_name: str):
         points: List[Point] = []
-        with open(file_name) as dataset_file:
-            reader = csv.DictReader(
-                dataset_file, fieldnames=('X', 'Y', 'Class'))
-            for point in reader:
-                points.append(Point(point["X"], point["Y"], point["Class"]))
+        reader = Csv.read_data(file_name, fields_names=(
+            'X', 'Y', 'Class'), separator="\t", delimiter=",")
+        for point in reader:
+            points.append(Point(point["X"], point["Y"], point["Class"]))
         return points
 
     @staticmethod
-    def showDataPoints(points: List[Point]):
+    def show_data(points: List[Point]):
         print(np.array([str(i) for i in points]).T)
 
-    ##Draws dots according to class
+    # Draws dots according to class
     @staticmethod
-    def show_data(points: List[Point]):
+    def show_colored_data(points: List[Point]):
         classColormap = ListedColormap(['#FF0000', '#00FF00'])
         pl.scatter([points[i].x for i in range(len(points))],
-                [points[i].y for i in range(len(points))],
-                c=[points[i].label for i in range(len(points))],
+                   [points[i].y for i in range(len(points))],
+                   c=[points[i].label for i in range(len(points))],
+                   cmap=classColormap)
+        pl.show()
+
+    @staticmethod
+    def show_difference(test_points: List[Point], predicted_labels: List[Point]):
+        classColormap = ListedColormap(['#FF0000', '#00FF00','#F08080','#90EE90'])
+        colors=[]
+
+        for i in range(len(predicted_labels)):
+            if (predicted_labels[i]==0 and test_points[i].label==0):
+                colors.append([0,50])
+            if (predicted_labels[i]==1 and test_points[i].label==1):
+                colors.append([1,50])
+            if (predicted_labels[i]==1 and test_points[i].label==0):
+                colors.append([2,50])
+            #when it's been predicted as red but it is actually green
+            if (predicted_labels[i]==0 and test_points[i].label==1):
+                colors.append([3,50])
+
+        pl.scatter([test_points[i].x for i in range(len(test_points))],
+                [test_points[i].y for i in range(len(test_points))],
+                c=[colors[i][0] for i in range(len(colors))],
+                s=[colors[i][1] for i in range(len(colors))],
                 cmap=classColormap)
         pl.show()
