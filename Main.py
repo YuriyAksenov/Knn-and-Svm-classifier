da36c9f2d3e295eb721edd6bd7c58305e46696d8
Yuriy Aksenov <Yura290610@yandex.ru>
2018-12-04 02:10:37 +0300

    svm поделал

--- Main.py
+++ Main.py
@@ -1,12 +1,39 @@
 
 from DataAnalyzer import DataAnalyzer
 from Data import Data
-
+from Svm import *
+import  numpy as np
+import random
+from Metrics import * 
 
 fileName = 'Chips.txt'
 data = Data.read_points_from_file(fileName)
-Data.show_data(data)
+# Data.show_data(data)
 DataAnalyzer.analyze_knn(data)
+# random.shuffle(data)
+# random.shuffle(data)
+# random.shuffle(data)
+# random.shuffle(data)
+# random.shuffle(data)
 
 
+# x = np.array([[item.x, item.y] for item in data])
+# y = np.array([-1 if item.label == 0 else 1 for item in data])
 
+# x_train = x[:100]
+# y_train = y[:100]
+# x_test = x[100:]
+# y_test = y[100:]
+
+# svm = SvmClassifier(C=1, kernel=polynomial_kernel)
+# svm.fit(x_train,y_train)
+# res = svm.predict(x_test)
+
+# res = np.array([0 if item == -1 else 1 for item in res])
+# y_test = np.array([0 if item == -1 else 1 for item in y_test])
+
+# print(res)
+# f = Metrics.f_score(y_test, res);
+
+# print('-------------------------------')
+# print(f)
--- Svm.py
+++ Svm.py
@@ -12,7 +12,7 @@ def polynomial_kernel(x, y, p=3):
 def gaussian_kernel(x, y, sigma=5.0):
     return np.exp(-linalg.norm(x-y)**2 / (2 * (sigma ** 2)))
 
-class SVM(object):
+class SvmClassifier:
 
     def __init__(self, kernel=linear_kernel, C=None):
         self.kernel = kernel
@@ -23,16 +23,20 @@ class SVM(object):
         n_samples, n_features = X.shape
 
         # Gram matrix
+        # считаем ядра. Они нужны если у нас нелинейное разделеление, например, как у нас два круга - то тут лучше и спользовать полиномальное ядро
         K = np.zeros((n_samples, n_samples))
         for i in range(n_samples):
             for j in range(n_samples):
                 K[i,j] = self.kernel(X[i], X[j])
 
+        # считаем  двойственную задачу программирования - находим минимум функции. Находим как должна пройти наша кривая, чтобы быть равноудаленной ото всех
+        # просто можнон исользоптьв функцию из либы для подсчета, так как самим сложно считать
         P = cvxopt.matrix(np.outer(y,y) * K)
         q = cvxopt.matrix(np.ones(n_samples) * -1)
-        A = cvxopt.matrix(y, (1,n_samples))
+        A = cvxopt.matrix(y, (1,n_samples), 'd')
         b = cvxopt.matrix(0.0)
 
+        # это вроде говорит, что надо ли использовать софт или не софт линию. Софт - это когда могут быть данные внутри полосы резделения
         if self.C is None:
             G = cvxopt.matrix(np.diag(np.ones(n_samples) * -1))
             h = cvxopt.matrix(np.zeros(n_samples))
@@ -48,8 +52,9 @@ class SVM(object):
         solution = cvxopt.solvers.qp(P, q, G, h, A, b)
 
         # Lagrange multipliers
-        a = np.ravel(solution['x'])
+        a = np.ravel(solution['x']) # наши иксы, минимальные - то есть при этих иксах наша функция наиболее приближена. Это даже не иксы, а веса, при которых фукнция наиболее приближена 
 
+        # отсекаем все, что маленькое
         # Support vectors have non zero lagrange multipliers
         sv = a > 1e-5
         ind = np.arange(len(a))[sv]
@@ -86,4 +91,25 @@ class SVM(object):
             return y_predict + self.b
 
     def predict(self, X):
+        # берем только знак от того, что получилось при предсказаниии +1 или -1
         return np.sign(self.project(X))
+
+    # @staticmethod
+    # def __calculate_kernel(distance: float, kernel_type: KernelType):
+    #     r = distance
+    #     # оптимальное Епанчиклва
+    #     if kernel_type == KernelType.E:
+    #         return 3/4*(1-r**2)
+    #     # квартическое
+    #     if kernel_type == KernelType.Q:
+    #         return 15/16*(1-r**2)**2
+    #     # теругольное
+    #     if kernel_type == KernelType.T:
+    #         return 1-abs(r)
+    #     # гуасовское
+    #     if kernel_type == KernelType.G:
+    #         return pow(2 * pi, (-0.5))*exp(-0.5*r**2)
+    #     # прямоугольное rectangle
+    #     if kernel_type == KernelType.R:
+    #         return 1/2
+    #     return 1
