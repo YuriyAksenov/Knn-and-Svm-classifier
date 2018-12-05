234f0f55003ab416f9491ad8219f1ace4d551f19
Yuriy Aksenov <Yura290610@yandex.ru>
2018-12-05 22:49:37 +0300

    Добавил p-value и кси квадрат распределение

--- Metrics.py
+++ Metrics.py
@@ -2,8 +2,63 @@ from typing import List
 
 
 class Metrics:
+
     @staticmethod
-    def get_metrics(real_data: list, predicted_data: list):
+    def p_value(real_data: list, predicted_data: list, labels: list):
+        degrees_of_freedom, x_square = Metrics.__get_x_square_distribution(
+            real_data, predicted_data, labels)
+        return Metrics.__p_value(degrees_of_freedom, x_square)
+
+    @staticmethod
+    def f_score(real_data: list, predicted_data: list):
+        """Get f score"""
+        metrics = Metrics.__get_metrics(real_data, predicted_data)
+
+        true_positive = metrics[0]
+        false_positive = metrics[1]
+        #true_negative = metrics[2]
+        false_negative = metrics[3]
+
+        precision = true_positive / (true_positive + false_positive)
+        recall = true_positive / (true_positive + false_negative)
+        fscore = 2*(precision*recall) / (precision+recall)
+
+        return (fscore)
+
+    @staticmethod
+    def plot_confusion_matrix(real_data: list, predicted_data: list):
+        import numpy as np
+        metrics = Metrics.__get_metrics(real_data, predicted_data)
+        aplot_confusion_matrix(cm=np.array([[metrics[0], metrics[1]],
+                                            [metrics[3], metrics[2]]]), normalize=True, target_names=["0", "1"], title="Confusion Matrix, Normalized")
+
+    @staticmethod
+    def __get_x_square_distribution(real_data: list, predicted_data: list, labels: list):
+        """Get  x^2 = Σ((o-e)2/e)"""
+        degrees_of_freedom = len(labels) - 1
+        result = 0.0
+        for label in labels:
+            # количество данных, принадлежащих данному классу реальных
+            real_count = len([x for x in real_data if x == label])
+            # количество данных, принадлежащих данному классу предсказанных
+            predicted_count = len([x for x in predicted_data if x == label])
+            result += (predicted_count - real_count)**2 / real_count
+        return (degrees_of_freedom, result)
+
+    @staticmethod
+    def __p_value(degrees_of_freedom: int, x_square_distribution: float):
+        table = Metrics.__p_value_table()
+        for index, x in enumerate(table[degrees_of_freedom]):
+            if x_square_distribution > x:
+                continue
+            if(index > 0):
+                return (table[0][index], table[0][index-1])
+            else:
+                return (table[0][index], table[0][index])
+
+    @staticmethod
+    def __get_metrics(real_data: list, predicted_data: list):
+        """Get metrics (true_positive, false_positive, true_negative, false_negative)"""
         small_number = 0.000000000000000001
         true_positive = small_number
         false_positive = small_number
@@ -22,26 +77,9 @@ class Metrics:
         return (true_positive, false_positive, true_negative, false_negative)
 
     @staticmethod
-    def f_score(real_data: list, predicted_data: list):
-        metrics = Metrics.get_metrics(real_data, predicted_data)
-
-        true_positive = metrics[0]
-        false_positive = metrics[1]
-        #true_negative = metrics[2]
-        false_negative = metrics[3]
-
-        precision = true_positive / (true_positive + false_positive)
-        recall = true_positive / (true_positive + false_negative)
-        fscore = 2*(precision*recall) / (precision+recall)
-
-        return (fscore)
-
-    @staticmethod
-    def plot_confusion_matrix(real_data:list, predicted_data: list):
-        import numpy as np
-        metrics = Metrics.get_metrics(real_data, predicted_data)
-        aplot_confusion_matrix(cm=np.array([[metrics[0], metrics[1]],
-                                            [metrics[3], metrics[2]]]), normalize=True, target_names=["0", "1"], title="Confusion Matrix, Normalized")
+    def __p_value_table():
+        return [[0.99, 0.975, 0.95, 0.90, 0.10, 0.05, 0.025, 0.01],
+            [0.000, 0.001, 0.004, 0.016, 2.706, 3.841, 5.024, 6.635]]
 
 
 def aplot_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=None, normalize=True):
@@ -85,3 +123,24 @@ def aplot_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=None
     plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(
         accuracy, misclass))
     plt.show()
+
+
+# import numpy as np
+
+# r1 = np.zeros((1, 100), dtype=np.int32).ravel().tolist()
+# r2 = np.ones((1, 50), dtype=np.int32).ravel().tolist()
+# a = r1+r2
+# p1 = np.zeros((1, 90), dtype=np.int32).ravel().tolist()
+# p2 = np.ones((1, 60), dtype=np.int32).ravel().tolist()
+# b = p1+p2
+
+# # x_square = Metrics.get_x_square_distribution(a, b, list([0, 1]))
+# # print(x_square)
+# # p = Metrics.p_value(x_square[0], x_square[1])
+# # print(p)
+# res = Metrics.p_value(a, b,  list([0, 1]))
+# print(res)
+# res = Metrics.f_score(a, b)
+# print(res)
+# res = Metrics.plot_confusion_matrix(a, b)
+# print(res)
